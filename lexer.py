52ab57ac799c26fcfc1ef8c264f52ca002f37766
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-08-24 21:30:58 -0500

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -1,6 +1,5 @@
 #Serrano Sanchez Bryant Ricardo
 #Compiladores - Lexer
-
 from sys import *
 
 #read content from file
@@ -8,54 +7,48 @@ def open_file(file):
 	data = open(file,"r").read()
 	return data
 
-
+#lex function
 def lex(content):
 	token = ""
+
 	flag_s = 0
 	string = ""
+
+	flag_i = 0
+	integer = ""
+
 	lexer = []
-	#save the content as list
+	#save the content of file as list
 	content = list(content)
 
 	
 	for char in content:
 		token += char
-		#not erase a space, tab or line jump when inside a string
 		if token == " " and flag_s != 1:
-			#print("found a white space")
-			token = ""
+			token = ""				
 
 		elif token == '\t' and flag_s != 1:
-			#print("found a tab")
 			token = ""
 
 		elif token == '\n' and flag_s != 1:
-			#print("found a jump line")
 			token = ""
-		
-		#case of an open or close brace
+			
 		elif token == "{":
 			new_item = []
 			new_item.append("openBrace")
 			new_item.append(token)
-			
-
 			lexer.append(new_item)
 			
-
 			token = ""
 
 		elif token == "}":
 			new_item = []
 			new_item.append("closeBrace")
 			new_item.append(token)
-
 			lexer.append(new_item)
-
 
 			token = ""
 
-		#case of an open or close parenthesis
 		elif token == "(":
 			new_item = []
 			new_item.append("openParenthesis")
@@ -72,16 +65,29 @@ def lex(content):
 			
 			token = ""		
 
-		#case of a semicolon
+		#when using multi-digit integers: untill lexer finds a semicolon, it inserts the integer	
 		elif token == ";":
+	
+			if integer != "" and flag_i == 0:
+
+				new_item = []
+				new_item.append("ID INT")
+				new_item.append(integer)
+				lexer.append(new_item)
+				integer = ""	
+
 			new_item = []
 			new_item.append("semicolon")
 			new_item.append(token)
 			lexer.append(new_item)
 			
-			token = ""			
+			"""elif expr != "" and isexpr == 0:
+				print(expr + "num")
+				expr = ""
+			"""
 
-		#case of declaring an int type
+			token = ""		
+				
 		elif token == "int":
 			new_item = []
 			new_item.append("INT KEYWORD")
@@ -90,7 +96,6 @@ def lex(content):
 			
 			token = ""
 
-		#case of declaring a returning value
 		elif token == "return":
 			new_item = []
 			new_item.append("RETURN KEYWORD")
@@ -98,92 +103,7 @@ def lex(content):
 			lexer.append(new_item)
 			
 			token = ""
-
-
-		#case in use of an int type variable(0-9)
-		elif token == "0":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
 			
-			token = ""
-
-
-		elif token == "1":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-		elif token == "2":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-		elif token == "3":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-		elif token == "4":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-		elif token == "5":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-		elif token == "6":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = "" 
-
-		elif token == "7":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-		elif token == "8":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = "" 
-
-		elif token == "9":
-			new_item = []
-			new_item.append("ID INT")
-			new_item.append(token)
-			lexer.append(new_item)
-			
-			token = ""
-
-
-		#case of using main keyword
 		elif token == "main":
 			new_item = []
 			new_item.append("MAIN KEYWORD")
@@ -192,8 +112,6 @@ def lex(content):
 			
 			token = ""
 
-
-		#case of using print keyword
 		elif token == "print":
 			new_item = []
 			new_item.append("PRINT KEYWORD")
@@ -202,31 +120,59 @@ def lex(content):
 			
 			token = ""
 
-
-		#case of declaring a string type
 		elif token == "\"":
+			
 			if flag_s == 0:
 				flag_s = 1
 			elif flag_s == 1:
+				new_item = []
 				new_item.append("ID STRING")
 				new_item.append(string)
 				lexer.append(new_item)
 				
-				
 				string = ""
 				flag_s = 0
+				token = ""
 
 		elif flag_s == 1:
 			string += char
 			token = ""
 
-	#tagging tokens
-	print(lexer)
+		elif token == "0" or token == "1" or token == "2" or token == "3" or token == "4" or token == "5" or token == "6" or token == "7" or token == "8" or token == "9":
+
+			integer += token
+			token = ""
+		
+
+		"""elif token == "+":
+			isexpr = 1
+			expr += token
+			token = ""
+		"""	
+	return lexer
 	
+
+def parse(tokens):
+
+
+	par = []
+
+	if len(tokens) == 0:
+		print("ERROR: SOURCE CODE EMPTY")
+
+	else:
+		for sublist in tokens:
+			if sublist[0] == 'MAIN KEYWORD':
+				print("found main")
+			
+
+
 def run():
     #file's name goes as second parameter in command line
 	data = open_file(argv[1])
-	lex(data)
+	print(lex(data))
+
+
 
 run() 
 
