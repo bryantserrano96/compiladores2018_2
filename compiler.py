a946386d91650c5d8befb9ec5544e9f28cdfdc6f
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-10-02 14:57:21 -0500

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -1,29 +1,36 @@
+#imports lex, parser and generate program
 import lex
 import parser
 import generate
 
+#import os to write in terminal
 import os
 
+#calls modules from anytree to reed tree from parser
 from anytree import Node, RenderTree, PreOrderIter
 
+#to read document from terminal
 from sys import*
 
-
+#function to open file
 def open_file(file):
 	data = open(file,"r").read()
 	return data
 
+#stores data from second argument in terminal
 data = open_file( argv[1] )
 
-#print ( lex.lexer(data) )
+#stores tree in "a"
+a = parser.parse_program( lex.lexer( data ) )
 
-a = parser.program( lex.lexer( data ) )
+#saves preorder items from tree in t
+t = [node.name[0] for node in PreOrderIter(a)]
 
-tree = [node.name[0] for node in PreOrderIter(a)]
+#saves number ( number in node "Cte")
+num = t[3]
 
-num = tree[3]
-
+#generate assembly program with number givin
 generate.write(num)
 
-
+#creates executabe assembly program
 os.system("gcc -m64 assembly.s -o out")
