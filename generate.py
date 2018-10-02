808142c21c2d1a38da11658445b6c568faa00da4
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-10-02 14:55:03 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,15 +1,14 @@
+#function to write an .s document
 def write(ast):
+	#creates or overwrites over assembly.s
 	f = open("assembly.s","w")
-
-
+	#line for .globl main
 	f.write("\t.globl main\n")
-
+	#line for main:
 	f.write("main:\n")
-
+	#line for movl $"cte", %eax
 	f.write("\tmovl\t$"+ast+", %eax\n")
-
+	#line for ret
 	f.write("\tret\n")
-		
-
+	#closes document
 	f.close()
-
