eeead2233c069607f181c32ad76f0bb642c077b6
bryantserrano96 <42443238+bryantserrano96@users.noreply.github.com>
2018-10-04 11:49:39 -0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,14 +1,24 @@
+#calls modules from anytree to reed tree from parser
+from anytree import Node, RenderTree, PreOrderIter
+
 #function to write an .s document
-def write(ast):
-	#creates or overwrites over assembly.s
-	f = open("assembly.s","w")
+def write( ast ):
+
+	#saves preorder items from tree in t
+	t = [node.name[0] for node in PreOrderIter( ast )]
+
+	#saves number
+	num = t[3]
+
+	#creates and overwrites over assembly.s
+	f = open( "assembly.s","w" )
 	#line for .globl main
-	f.write("\t.globl main\n")
+	f.write( "\t.globl main\n" )
 	#line for main:
-	f.write("main:\n")
+	f.write( "main:\n" )
 	#line for movl $"cte", %eax
-	f.write("\tmovl\t$"+ast+", %eax\n")
+	f.write( "\tmovl\t$"+num+", %eax\n" )
 	#line for ret
-	f.write("\tret\n")
+	f.write( "\tret\n" )
 	#closes document
 	f.close()
